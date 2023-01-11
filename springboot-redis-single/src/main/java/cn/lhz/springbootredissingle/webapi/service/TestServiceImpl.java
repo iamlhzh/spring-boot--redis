7f9e6f61d2147988b45dea34d5124313c045850c
ianlhzh <1187563558@qq.com>
2023-01-12 01:07:05 +0800

    配置文件

--- springboot-redis-single/src/main/java/cn/lhz/springbootredissingle/webapi/service/TestServiceImpl.java
+++ springboot-redis-single/src/main/java/cn/lhz/springbootredissingle/webapi/service/TestServiceImpl.java
@@ -1,10 +1,14 @@
 package cn.lhz.springbootredissingle.webapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author 卢宏政
@@ -12,12 +16,18 @@ import java.util.Map;
  */
 @Service
 public class TestServiceImpl implements TestService {
+    
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Override
     public Map<String, Object> getTest(HttpServletRequest request) {
         Map<String,Object>map = new HashMap<>();
         map.put("RemoteAddr",request.getRemoteAddr());
         map.put("RemoteHost",request.getRemoteHost());
+        Boolean a = redisTemplate.hasKey("a");
+        Set keys = redisTemplate.keys("*");
+        ValueOperations valueOperations = redisTemplate.opsForValue();
         return map;
     }
 }
