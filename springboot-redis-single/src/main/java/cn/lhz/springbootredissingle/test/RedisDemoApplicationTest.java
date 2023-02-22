4109580772a56e9a4187779e980ad841c526fc39
ianlhzh <1187563558@qq.com>
2023-02-22 08:26:01 +0800

    1

--- springboot-redis-single/src/main/java/cn/lhz/springbootredissingle/test/RedisDemoApplicationTest.java
+++ springboot-redis-single/src/main/java/cn/lhz/springbootredissingle/test/RedisDemoApplicationTest.java
@@ -4,7 +4,7 @@ package cn.lhz.springbootredissingle.test;
 
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.DefaultTypedTuple;
